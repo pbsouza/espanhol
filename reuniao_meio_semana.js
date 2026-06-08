41c58ada0b235a07b9ce74a88501069563bad205
Pedro Belchior de Souza <64044028+pbsouza@users.noreply.github.com>
2026-06-08 06:15:05 -0300

    Update reuniao_meio_semana.js

--- reuniao_meio_semana.js
+++ reuniao_meio_semana.js
@@ -201,7 +201,7 @@ function exibirTabelaReuniao(dados, dataId) {
   htmlMinisterio += `</div>`; 
 
   // 4. Bloco: Nossa Vida Cristã (Geração Dinâmica)
-let htmlVida = `<h3 class="secao-titulo vida-crista-cor">🐑 Nossa Vida Cristã</h3>`;
+let htmlVida = `<h3 class="secao-titulo bg-vida">🐑 Nossa Vida Cristã</h3>`;
 
   // 1. Renderiza primeiro as partes variáveis (ex: Necessidades Locais)
   if (dados.partesVida && dados.partesVida.length > 0) {
